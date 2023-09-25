093ce9dcfc9633f12b125a1380c0aed4b0f0f92b
IslamDeVFull <it.front.back@gmail.com>
2023-09-25 12:47:33 +0500

    0.2.8v

---
+++ 1-HTML/Mashq/test/baseDePreguntas.js
@@ -0,0 +1,62 @@
+baseDePreguntas = [
+  {
+    pregunta: `<b class="number">1)</b>HTML nimani anglatadi?`,
+    respuesta: ` Giperhavolalar va matnni belgilash tili`,
+    distractores: 
+    [
+    ` Bosh sahifa vositalarini belgilash tili`,
+    ` Gipermatn belgilash tili `,
+    ` To'g'ri javob yo'q`
+  ],
+  },
+  {
+    pregunta: `<b class="number">2)</b> Eng katta sarlavha uchun to'g'ri HTML elementini tanlang...`,
+    respuesta: `<code>&lt;</code><code>h1</code><code>&gt;</code>`,
+    distractores: 
+    [
+    `  <code>&lt;</code><code>heading</code><code>&gt;</code>`,
+    `  <code>&lt;</code><code>head</code><code>&gt;</code>`,
+    ` <code>&lt;</code><code>h6</code><code>&gt;</code>`
+  ],
+  },
+  {
+    pregunta: `<b class="number">3)</b> Satr uzilishini kiritish uchun to'g'ri HTML elementi nima?`,
+    respuesta: `  <code>&lt;</code><code>br</code><code>&gt;</code>`,
+    distractores: 
+    [
+    `  <code>&lt;</code><code>break</code><code>&gt;</code>`,
+    `  <code>&lt;</code><code>lb</code><code>&gt;</code>`,
+    `  <code>&lt;</code><code>ul</code><code>&gt;</code>`
+  ],
+  },
+  {
+    pregunta: `<b class="number">4)</b> Yakuniy tegni ko'rsatish uchun qaysi belgi ishlatiladi?`,
+    respuesta: ` /`,
+    distractores: 
+    [
+    `^`,
+    ` <`,
+    `*`
+  ],
+  },
+  {
+    pregunta: `<b class="number">5)</b> Qaysi HTML elementi saytni sarlavhasini belgilaydi?`,
+    respuesta: `   <code>&lt;title&gt;</code>`,
+    distractores: 
+    [
+    `  <code>&lt;meta&gt;</code>`,
+    ` <code>&lt;head&gt;</code>`,
+    ` <code>&lt;h1&gt;</code>`
+  ]
+},
+  {
+    pregunta: `<b class="number">6)</b> HTML5 uchun qaysi hujjat turi to'g'ri?`,
+    respuesta: `   <code>&lt;!DOCTYPE html&gt;</code>`,
+    distractores: 
+    [
+    `  <code>&lt;!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 5.0//EN" "http://www.w3.org/TR/html5/strict.dtd"&gt;</code>`,
+    ` <code>&lt;!DOCTYPE HTML5&gt;</code>`,
+    ` <code>&lt;html&gt;</code>`
+  ]
+}
+];
