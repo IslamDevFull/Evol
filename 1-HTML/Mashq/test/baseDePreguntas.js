39a55aa50495098ed0b9d3147d75fc8ecb298cda
IslamDevFull <it.front.back@gmail.com>
2023-11-06 19:34:31 +0100

    0.2.16v

--- 1-HTML/Mashq/test/baseDePreguntas.js
+++ 1-HTML/Mashq/test/baseDePreguntas.js
@@ -11,52 +11,302 @@ baseDePreguntas = [
   },
   {
     pregunta: `<b class="number">2)</b> Eng katta sarlavha uchun to'g'ri HTML elementini tanlang...`,
-    respuesta: `<kbd>&lt;</kbd><kbd class="teg">h1</kbd><kbd>&gt;</kbd>`,
+    respuesta: `<code>&lt;</code><kbd class="teg">h1</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `  <kbd>&lt;</kbd><kbd class="teg">heading</kbd><kbd>&gt;</kbd>`,
-    `  <kbd>&lt;</kbd><kbd class="teg">head</kbd><kbd>&gt;</kbd>`,
-    ` <kbd>&lt;</kbd><kbd class="teg">h6</kbd><kbd>&gt;</kbd>`
+    `  <code>&lt;</code><kbd class="teg">heading</kbd><code>&gt;</code>`,
+    `  <code>&lt;</code><kbd class="teg">head</kbd><code>&gt;</code>`,
+    ` <code>&lt;</code><kbd class="teg">h6</kbd><code>&gt;</code>`
   ],
   },
   {
     pregunta: `<b class="number">3)</b> Satr uzilishini kiritish uchun to'g'ri HTML elementi nima?`,
-    respuesta: `  <kbd>&lt;</kbd><kbd class="teg">br</kbd><kbd>&gt;</kbd>`,
+    respuesta: `  <code>&lt;</code><kbd class="teg">br</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `  <kbd>&lt;</kbd><kbd class="teg">break</kbd><kbd>&gt;</kbd>`,
-    `  <kbd>&lt;</kbd><kbd class="teg">lb</kbd><kbd>&gt;</kbd>`,
-    `  <kbd>&lt;</kbd><kbd class="teg">ul</kbd><kbd>&gt;</kbd>`
+    `  <code>&lt;</code><kbd class="teg">break</kbd><code>&gt;</code>`,
+    `  <code>&lt;</code><kbd class="teg">lb</kbd><code>&gt;</code>`,
+    `  <code>&lt;</code><kbd class="teg">ul</kbd><code>&gt;</code>`
   ],
   },
   {
     pregunta: `<b class="number">4)</b> Yakuniy tegni ko'rsatish uchun qaysi belgi ishlatiladi?`,
-    respuesta: `<kbd>/</kbd>`,
+    respuesta: `<code>/</code>`,
     distractores: 
     [
-    `<kbd>^</kbd>`,
-    `<kbd><</kbd>`,
-    `<kbd>*</kbd>`
+    `<code>^</code>`,
+    `<code><</code>`,
+    `<code>*</code>`
   ],
   },
   {
     pregunta: `<b class="number">5)</b> Qaysi HTML elementi saytni sarlavhasini belgilaydi?`,
-    respuesta: `   <kbd>&lt;<kbd class="teg">title</kbd>&gt;</kbd>`,
+    respuesta: `   <code>&lt;</code><kbd class="teg">title</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `  <kbd>&lt;<kbd class="teg">meta</kbd>&gt;</kbd>`,
-    ` <kbd>&lt;<kbd class="teg">head</kbd>&gt;</kbd>`,
-    ` <kbd>&lt;<kbd class="teg">h1</kbd>&gt;</kbd>`
+    `  <code>&lt;</code><kbd class="teg">meta</kbd><code>&gt;</code>`,
+    ` <code>&lt;</code><kbd class="teg">head</kbd><code>&gt;</code>`,
+    ` <code>&lt;</code><kbd class="teg">h1</kbd><code>&gt;</code>`
   ]
 },
   {
     pregunta: `<b class="number">6)</b> HTML5 uchun qaysi hujjat turi to'g'ri?`,
-    respuesta: `   <kbd>&lt;<kbd class="teg">!DOCTYPE</kbd> <kbd class="atr">html</kbd>&gt;</kbd>`,
+    respuesta: `<code>&lt;</code><kbd class="teg">!DOCTYPE</kbd> <kbd class="atr">html</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `  <kbd>&lt;<kbd class="teg">!DOCTYPE</kbd> <kbd class="atr">HTML PUBLIC</kbd> <kbd class="str">"-//W3C//DTD HTML 5.0//EN" "http://www.w3.org/TR/html5/strict.dtd"</kbd>&gt;</kbd>`,
-    ` <kbd>&lt;<kbd class="teg">!DOCTYPE</kbd> <kbd class="atr">HTML5</kbd>&gt;</kbd>`,
-    ` <kbd>&lt;<kbd class="teg">html</kbd>&gt;</kbd>`
+    `  <code>&lt;</code><kbd class="teg">!DOCTYPE</kbd> <kbd class="atr">HTML PUBLIC</kbd> <kbd class="str">"-//W3C//DTD HTML 5.0//EN" "http://www.w3.org/TR/html5/strict.dtd"</kbd><code>&gt;</code>`,
+    ` <code>&lt;</code><kbd class="teg">!DOCTYPE</kbd> <kbd class="atr">HTML5</kbd><code>&gt;</code>`,
+    ` <code>&lt;</code><kbd class="teg">html</kbd><code>&gt;</code>`
+  ]
+},
+  {
+    pregunta: `<b class="number">7)</b> Veb standartlarini kim ishlab chiqaradi?`,
+    respuesta: `World Wide Web Konsortsiumi`,
+    distractores: 
+    [
+    `Google`,
+    `Mozilla`,
+    `Microsoft`
+  ]
+},
+  {
+    pregunta: `<b class="number">8)</b> Muhim matnni aniqlash uchun (matni qalin qilish uchun) to'g'ri HTML elementini tanlang`,
+    respuesta: `&lt;strong&gt;`,
+    distractores: 
+    [
+    `&lt;b&gt;`,
+    `&lt;important&gt;`,
+    `&lt;i&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">9)</b> Yangi matn qo'shilganini aniqlash uchun to'g'ri HTML elementini tanlang`,
+    respuesta: `&lt;em&gt;`,
+    distractores: 
+    [
+    `&lt;italic&gt;`,
+    `&lt;i&gt;`,
+    `&lt;strong&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">10)</b> Giperhavola (Silka) yaratish uchun to'g'ri HTML nima?`,
+    respuesta: `&lt;a href="http://www.w3schools.com"&gt;W3Schools&lt;/a&gt;`,
+    distractores: 
+    [
+    `&lt;a url="http://www.w3schools.com"&gt;W3Schools.com&lt;/a&gt;`,
+    `&lt;a&gt;http://www.w3schools.com&lt;/a&gt;`,
+    `&lt;a name="http://www.w3schools.com"&gt;W3Schools.com&lt;/a&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">11)</b> Brauzer oynasida Yangi havola oynasini qanday ochish mumkin?`,
+    respuesta: `&lt;a href="url" target="_blank"&gt;`,
+    distractores: 
+    [
+    `&lt;a href="url" new&gt;`,
+    `&lt;a href="url" target="new"&gt;`,
+    `&lt;a href="#" target="new #"&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">12)</b> Ushbu elementlarning qaysi biri &lt;jadval&gt; yaratish uchun asosiy elementlar deb hisoblanadi ?`,
+    respuesta: `&lt;table&gt;&lt;tr&gt;&lt;td&gt;`,
+    distractores: 
+    [
+    `&lt;table&gt;&lt;tr&gt;&lt;tt&gt;`,
+    `&lt;table&gt;&lt;head&gt;&lt;tfoot&gt;`,
+    `&lt;thead&gt;&lt;body&gt;&lt;tr&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">13)</b> Qanday qilib raqamlangan ro'yxatni tuzish mumkin?`,
+    respuesta: `&lt;ol&gt;`,
+    distractores: 
+    [
+    `&lt;list&gt;`,
+    `&lt;dl&gt;`,
+    `&lt;ul&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">14)</b> Belgilangan ro'yxatni qanday qilib tuzish mumkin?`,
+    respuesta: `&lt;ul&gt;`,
+    distractores: 
+    [
+    `&lt;ol&gt;`,
+    `&lt;dl&gt;`,
+    `&lt;list&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">15)</b> Belgilash katakchasini qo'yish uchun to'g'ri HTML nima?`,
+    respuesta: `&lt;input type="checkbox"&gt;`,
+    distractores: 
+    [
+    `&lt;input type="check"&gt;`,
+    `&lt;check&gt;`,
+    `&lt;checkbox&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">16)</b> Matn kiritish maydonini yaratish uchun to'g'ri HTML nima?`,
+    respuesta: `&lt;input type="text"&gt;`,
+    distractores: 
+    [
+    `&lt;textfield&gt;`,
+    `&lt;textinput type="text"&gt;`,
+    `&lt;input type="textfield"&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">17)</b> Ochiladigan ro'yxatni tuzish uchun to'g'ri HTML nima?`,
+    respuesta: `&lt;select&gt;`,
+    distractores: 
+    [
+    `&lt;input type="dropdown"&gt;`,
+    `&lt;input type="list"&gt;`,
+    `&lt;list&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">17)</b> Katta matn maydonini yaratish uchun to'g'ri HTML kodi nima?`,
+    respuesta: `&lt;textarea&gt;`,
+    distractores: 
+    [
+    `&lt;input type="textarea"&gt;`,
+    `&lt;input type="textbox"&gt;`,
+    `&lt;text&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">18)</b> Rasm qo'shish uchun to'g'ri HTML kodi nima?`,
+    respuesta: `&lt;img src="image.gif" alt="MyImage"&gt;`,
+    distractores: 
+    [
+    `&lt;image src="image.gif" alt="MyImage"&gt;`,
+    `&lt;img alt="MyImage"&gt;image.gif&lt;/img&gt;`,
+    `&lt;img href="image.gif" alt="MyImage"&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">19)</b> &lt;iframe&gt; veb-sahifa ichida veb-sahifani ko'rsatish uchun ishlatiladimi ?.`,
+    respuesta: `To'g'ri`,
+    distractores: 
+    [
+    `&lt;iframe&gt; degan kod yo'q`,
+    `Yolg'on`,
+    `Foydalanuvchi tugmani bosganda`
+  ]
+},
+  {
+    pregunta: `<b class="number">20)</b> HTML da izoh yaratish uchun qaysi kod ishlatiladi ?`,
+    respuesta: `&lt;!-- --&gt;`,
+    distractores: 
+    [
+    `/* */`,
+    `//`,
+    `#`
+  ]
+},
+  {
+    pregunta: `<b class="number">21)</b> Rasmni ko'rsatish imkoni bo'lmasa, qaysi HTML atributi rasmni o'rnida matnni ko'rsatadi?`,
+    respuesta: `alt`,
+    distractores: 
+    [
+    `src`,
+    `title`,
+    `longdesc`
+  ]
+},
+  {
+    pregunta: `<b class="number">22)</b> Sayt uchun pastki bo'limni yaratish uchun qaysi HTML elementi qo'llaniladi?`,
+    respuesta: `&lt;footer&gt;`,
+    distractores: 
+    [
+    `&lt;bottom&gt;`,
+    `&lt;section&gt;`,
+    `&lt;div&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">23)</b> Video fayllarni o'rnatish uchun to'g'ri HTML elementi nima?`,
+    respuesta: `&lt;video&gt;`,
+    distractores: 
+    [
+    `&lt;media&gt;`,
+    `&lt;movie&gt;`,
+    `&lt;source&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">24)</b> Audio fayllarni o'rnatish uchun to'g'ri HTML elementi nima?`,
+    respuesta: `&lt;audio&gt;`,
+    distractores: 
+    [
+    `&lt;sound&gt;`,
+    `&lt;mp3&gt;`,
+    `&lt;source&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">25)</b> HTMLda onblur va onfocus:`,
+    respuesta: `Hodisa atributlari`,
+    distractores: 
+    [
+    `HTML elementlari`,
+    `Uslub atributlari`,
+    `CSS kodlari`
+  ]
+},
+  {
+    pregunta: `<b class="number">26)</b> HTMLda kiritish maydonini to'ldirish kerakligini ko'rsatish uchun qaysi atributdan foydalaniladi?`,
+    respuesta: `required`,
+    distractores: 
+    [
+    `placeholder`,
+    `formvalidate`,
+    `validate`
+  ]
+},
+  {
+    pregunta: `<b class="number">27)</b> O'lchov moslamasini chiqarish uchun qaysi HTML elementi ishlatiladi?`,
+    respuesta: `&lt;meter&gt;`,
+    distractores: 
+    [
+    `&lt;measure&gt;`,
+    `&lt;gauge&gt;`,
+    `&lt;range&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">28)</b> Qaysi HTML elementi menyu navigatsiya yaratish uchun xamma havolalarni o'z ichiga oladi?`,
+    respuesta: `&lt;nav&gt;`,
+    distractores: 
+    [
+    `&lt;navigate&gt;`,
+    `&lt;navigation&gt;`,
+    `&lt;a href="#"&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">29)</b> HTML kodini bosh qism bo'limni belgilash uchun qaysi HTML elementi qo'llaniladi?`,
+    respuesta: `&lt;head&gt;`,
+    distractores: 
+    [
+    `&lt;header&gt;`,
+    `&lt;section&gt;`,
+    `&lt;top&gt;`
+  ]
+},
+  {
+    pregunta: `<b class="number">30)</b> Saytning bosh qismini yaratish uchun qaysi HTML elementi qo'llaniladi?`,
+    respuesta: `&lt;header&gt;`,
+    distractores: 
+    [
+    `&lt;section&gt;`,
+    `&lt;head&gt;`,
+    `&lt;top&gt;`
   ]
 }
 ];
