fc5f09d4978895f8c3340ddec3629d900d3c6f50
IslamDevFull <it.front.back@gmail.com>
2023-11-29 18:05:22 +0100

    0.2.20v

--- 1-HTML/Mashq/test/baseDePreguntas.js
+++ 1-HTML/Mashq/test/baseDePreguntas.js
@@ -31,12 +31,12 @@ baseDePreguntas = [
   },
   {
     pregunta: `<b class="number">4)</b> Yakuniy tegni ko'rsatish uchun qaysi belgi ishlatiladi?`,
-    respuesta: `<code>/</code>`,
+    respuesta: `/`,
     distractores: 
     [
-    `<code>^</code>`,
-    `<code><</code>`,
-    `<code>*</code>`
+    `^`,
+    `<`,
+    `*`
   ],
   },
   {
@@ -51,11 +51,11 @@ baseDePreguntas = [
 },
   {
     pregunta: `<b class="number">6)</b> HTML5 uchun qaysi hujjat turi to'g'ri?`,
-    respuesta: `<code>&lt;</code><kbd class="teg">!DOCTYPE</kbd> <kbd class="atr">html</kbd><code>&gt;</code>`,
+    respuesta: `<code>&lt;</code><kbd class="teg">!DOCTYPE <kbd class="atr">html</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `  <code>&lt;</code><kbd class="teg">!DOCTYPE</kbd> <kbd class="atr">HTML PUBLIC</kbd> <kbd class="str">"-//W3C//DTD HTML 5.0//EN" "http://www.w3.org/TR/html5/strict.dtd"</kbd><code>&gt;</code>`,
-    ` <code>&lt;</code><kbd class="teg">!DOCTYPE</kbd> <kbd class="atr">HTML5</kbd><code>&gt;</code>`,
+    `  <code>&lt;</code><kbd class="teg">!DOCTYPE <kbd class="atr">HTML PUBLIC</kbd> <kbd class="str">"-//W3C//DTD HTML 5.0//EN" "http://www.w3.org/TR/html5/strict.dtd"</kbd></kbd><code>&gt;</code>`,
+    ` <code>&lt;</code><kbd class="teg">!DOCTYPE <kbd class="atr">HTML5</kbd><code>&gt;</code>`,
     ` <code>&lt;</code><kbd class="teg">html</kbd><code>&gt;</code>`
   ]
 },
@@ -71,137 +71,137 @@ baseDePreguntas = [
 },
   {
     pregunta: `<b class="number">8)</b> Muhim matnni aniqlash uchun (matni qalin qilish uchun) to'g'ri HTML elementini tanlang`,
-    respuesta: `&lt;strong&gt;`,
+    respuesta: `<code>&lt;</code><kbd class="teg">strong</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;b&gt;`,
-    `&lt;important&gt;`,
-    `&lt;i&gt;`
+    `<code>&lt;</code><kbd class="teg">b</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">important</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">i</kbd><code>&gt;</code>`
   ]
 },
   {
     pregunta: `<b class="number">9)</b> Yangi matn qo'shilganini aniqlash uchun to'g'ri HTML elementini tanlang`,
-    respuesta: `&lt;em&gt;`,
+    respuesta: `<code>&lt;</code><kbd class="teg">em</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;italic&gt;`,
-    `&lt;i&gt;`,
-    `&lt;strong&gt;`
+    `<code>&lt;</code><kbd class="teg">italic</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">i</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">strong</kbd><code>&gt;</code>`
   ]
 },
   {
     pregunta: `<b class="number">10)</b> Giperhavola (Silka) yaratish uchun to'g'ri HTML nima?`,
-    respuesta: `&lt;a href="http://www.w3schools.com"&gt;W3Schools&lt;/a&gt;`,
+    respuesta: `<code>&lt;</code><kbd class="teg">a href="http://www.w3schools.com"</kbd><code>&gt;</code>W3Schools<code>&lt;</code><kbd class="teg">/a</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;a url="http://www.w3schools.com"&gt;W3Schools.com&lt;/a&gt;`,
-    `&lt;a&gt;http://www.w3schools.com&lt;/a&gt;`,
-    `&lt;a name="http://www.w3schools.com"&gt;W3Schools.com&lt;/a&gt;`
+    `<code>&lt;</code><kbd class="teg">a url="http://www.w3schools.com"</kbd><code>&gt;</code>W3Schools.com<code>&lt;</code><kbd class="teg">/a</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">a</kbd><code>&gt;</code>http://www.w3schools.com<code>&lt;</code><kbd class="teg">/a</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">a name="http://www.w3schools.com"</kbd><code>&gt;</code>W3Schools.com<code>&lt;</code><kbd class="teg">/a</kbd><code>&gt;</code>`
   ]
 },
   {
     pregunta: `<b class="number">11)</b> Brauzer oynasida Yangi havola oynasini qanday ochish mumkin?`,
-    respuesta: `&lt;a href="url" target="_blank"&gt;`,
+    respuesta: `<code>&lt;</code><kbd class="teg">a href="url" target="_blank"</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;a href="url" new&gt;`,
-    `&lt;a href="url" target="new"&gt;`,
-    `&lt;a href="#" target="new #"&gt;`
+    `<code>&lt;</code><kbd class="teg">a href="url" new</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">a href="url" target="new"</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">a href="#" target="new #"</kbd><code>&gt;</code>`
   ]
 },
   {
-    pregunta: `<b class="number">12)</b> Ushbu elementlarning qaysi biri &lt;jadval&gt; yaratish uchun asosiy elementlar deb hisoblanadi ?`,
-    respuesta: `&lt;table&gt;&lt;tr&gt;&lt;td&gt;`,
+    pregunta: `<b class="number">12)</b> Ushbu elementlarning qaysi biri <code>&lt;</code><kbd class="teg">jadval</kbd><code>&gt;</code> yaratish uchun asosiy elementlar deb hisoblanadi ?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">table</kbd><code>&gt;</code><code>&lt;</code><kbd class="teg">tr</kbd><code>&gt;</code><code>&lt;</code><kbd class="teg">td</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;table&gt;&lt;tr&gt;&lt;tt&gt;`,
-    `&lt;table&gt;&lt;head&gt;&lt;tfoot&gt;`,
-    `&lt;thead&gt;&lt;body&gt;&lt;tr&gt;`
+    `<code>&lt;</code><kbd class="teg">table</kbd><code>&gt;</code><code>&lt;</code><kbd class="teg">tr</kbd><code>&gt;</code><code>&lt;</code><kbd class="teg">tt</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">table</kbd><code>&gt;</code><code>&lt;</code><kbd class="teg">head</kbd><code>&gt;</code><code>&lt;</code><kbd class="teg">tfoot</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">thead</kbd><code>&gt;</code><code>&lt;</code><kbd class="teg">body</kbd><code>&gt;</code><code>&lt;</code><kbd class="teg">tr</kbd><code>&gt;</code>`
   ]
 },
   {
     pregunta: `<b class="number">13)</b> Qanday qilib raqamlangan ro'yxatni tuzish mumkin?`,
-    respuesta: `&lt;ol&gt;`,
+    respuesta: `<code>&lt;</code><kbd class="teg">ol</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;list&gt;`,
-    `&lt;dl&gt;`,
-    `&lt;ul&gt;`
+    `<code>&lt;</code><kbd class="teg">list</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">dl</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">ul</kbd><code>&gt;</code>`
   ]
 },
   {
     pregunta: `<b class="number">14)</b> Belgilangan ro'yxatni qanday qilib tuzish mumkin?`,
-    respuesta: `&lt;ul&gt;`,
+    respuesta: `<code>&lt;</code><kbd class="teg">ul</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;ol&gt;`,
-    `&lt;dl&gt;`,
-    `&lt;list&gt;`
+    `<code>&lt;</code><kbd class="teg">ol</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">dl</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">list</kbd><code>&gt;</code>`
   ]
 },
   {
     pregunta: `<b class="number">15)</b> Belgilash katakchasini qo'yish uchun to'g'ri HTML nima?`,
-    respuesta: `&lt;input type="checkbox"&gt;`,
+    respuesta: `<code>&lt;</code><kbd class="teg">input type="checkbox"</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;input type="check"&gt;`,
-    `&lt;check&gt;`,
-    `&lt;checkbox&gt;`
+    `<code>&lt;</code><kbd class="teg">input type="check"</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">check</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">checkbox</kbd><code>&gt;</code>`
   ]
 },
   {
     pregunta: `<b class="number">16)</b> Matn kiritish maydonini yaratish uchun to'g'ri HTML nima?`,
-    respuesta: `&lt;input type="text"&gt;`,
+    respuesta: `<code>&lt;</code><kbd class="teg">input type="text"</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;textfield&gt;`,
-    `&lt;textinput type="text"&gt;`,
-    `&lt;input type="textfield"&gt;`
+    `<code>&lt;</code><kbd class="teg">textfield</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">textinput type="text"</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">input type="textfield"</kbd><code>&gt;</code>`
   ]
 },
   {
     pregunta: `<b class="number">17)</b> Ochiladigan ro'yxatni tuzish uchun to'g'ri HTML nima?`,
-    respuesta: `&lt;select&gt;`,
+    respuesta: `<code>&lt;</code><kbd class="teg">select</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;input type="dropdown"&gt;`,
-    `&lt;input type="list"&gt;`,
-    `&lt;list&gt;`
+    `<code>&lt;</code><kbd class="teg">input type="dropdown"</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">input type="list"</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">list</kbd><code>&gt;</code>`
   ]
 },
   {
-    pregunta: `<b class="number">17)</b> Katta matn maydonini yaratish uchun to'g'ri HTML kodi nima?`,
-    respuesta: `&lt;textarea&gt;`,
+    pregunta: `<b class="number">18)</b> Katta matn maydonini yaratish uchun to'g'ri HTML kodi nima?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">textarea</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;input type="textarea"&gt;`,
-    `&lt;input type="textbox"&gt;`,
-    `&lt;text&gt;`
+    `<code>&lt;</code><kbd class="teg">input type="textarea"</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">input type="textbox"</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">text</kbd><code>&gt;</code>`
   ]
 },
   {
-    pregunta: `<b class="number">18)</b> Rasm qo'shish uchun to'g'ri HTML kodi nima?`,
-    respuesta: `&lt;img src="image.gif" alt="MyImage"&gt;`,
+    pregunta: `<b class="number">19)</b> Rasm qo'shish uchun to'g'ri HTML kodi nima?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">img src="image.gif" alt="MyImage"</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;image src="image.gif" alt="MyImage"&gt;`,
-    `&lt;img alt="MyImage"&gt;image.gif&lt;/img&gt;`,
-    `&lt;img href="image.gif" alt="MyImage"&gt;`
+    `<code>&lt;</code><kbd class="teg">image src="image.gif" alt="MyImage"</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">img alt="MyImage"</kbd><code>&gt;</code>image.gif<code>&lt;</code><kbd class="teg">/img</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">img href="image.gif" alt="MyImage"</kbd><code>&gt;</code>`
   ]
 },
   {
-    pregunta: `<b class="number">19)</b> &lt;iframe&gt; veb-sahifa ichida veb-sahifani ko'rsatish uchun ishlatiladimi ?.`,
+    pregunta: `<b class="number">20)</b> <code>&lt;</code><kbd class="teg">iframe</kbd><code>&gt;</code> veb-sahifa ichida veb-sahifani ko'rsatish uchun ishlatiladimi ?.`,
     respuesta: `To'g'ri`,
     distractores: 
     [
-    `&lt;iframe&gt; degan kod yo'q`,
+    `<code>&lt;</code><kbd class="teg">iframe</kbd><code>&gt;</code> degan kod yo'q`,
     `Yolg'on`,
     `Foydalanuvchi tugmani bosganda`
   ]
 },
   {
-    pregunta: `<b class="number">20)</b> HTML da izoh yaratish uchun qaysi kod ishlatiladi ?`,
-    respuesta: `&lt;!-- --&gt;`,
+    pregunta: `<b class="number">21)</b> HTML da izoh yaratish uchun qaysi kod ishlatiladi ?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">!-- --</kbd><code>&gt;</code>`,
     distractores: 
     [
     `/* */`,
@@ -210,7 +210,7 @@ baseDePreguntas = [
   ]
 },
   {
-    pregunta: `<b class="number">21)</b> Rasmni ko'rsatish imkoni bo'lmasa, qaysi HTML atributi rasmni o'rnida matnni ko'rsatadi?`,
+    pregunta: `<b class="number">22)</b> Rasmni ko'rsatish imkoni bo'lmasa, qaysi HTML atributi rasmni o'rnida matnni ko'rsatadi?`,
     respuesta: `alt`,
     distractores: 
     [
@@ -220,37 +220,37 @@ baseDePreguntas = [
   ]
 },
   {
-    pregunta: `<b class="number">22)</b> Sayt uchun pastki bo'limni yaratish uchun qaysi HTML elementi qo'llaniladi?`,
-    respuesta: `&lt;footer&gt;`,
+    pregunta: `<b class="number">23)</b> Sayt uchun pastki bo'limni yaratish uchun qaysi HTML elementi qo'llaniladi?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">footer</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;bottom&gt;`,
-    `&lt;section&gt;`,
-    `&lt;div&gt;`
+    `<code>&lt;</code><kbd class="teg">bottom</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">section</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">div</kbd><code>&gt;</code>`
   ]
 },
   {
-    pregunta: `<b class="number">23)</b> Video fayllarni o'rnatish uchun to'g'ri HTML elementi nima?`,
-    respuesta: `&lt;video&gt;`,
+    pregunta: `<b class="number">24)</b> Video fayllarni o'rnatish uchun to'g'ri HTML elementi nima?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">video</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;media&gt;`,
-    `&lt;movie&gt;`,
-    `&lt;source&gt;`
+    `<code>&lt;</code><kbd class="teg">media</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">movie</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">source</kbd><code>&gt;</code>`
   ]
 },
   {
-    pregunta: `<b class="number">24)</b> Audio fayllarni o'rnatish uchun to'g'ri HTML elementi nima?`,
-    respuesta: `&lt;audio&gt;`,
+    pregunta: `<b class="number">25)</b> Audio fayllarni o'rnatish uchun to'g'ri HTML elementi nima?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">audio</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;sound&gt;`,
-    `&lt;mp3&gt;`,
-    `&lt;source&gt;`
+    `<code>&lt;</code><kbd class="teg">sound</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">mp3</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">source</kbd><code>&gt;</code>`
   ]
 },
   {
-    pregunta: `<b class="number">25)</b> HTMLda onblur va onfocus:`,
+    pregunta: `<b class="number">26)</b> HTMLda onblur va onfocus:`,
     respuesta: `Hodisa atributlari`,
     distractores: 
     [
@@ -260,7 +260,7 @@ baseDePreguntas = [
   ]
 },
   {
-    pregunta: `<b class="number">26)</b> HTMLda kiritish maydonini to'ldirish kerakligini ko'rsatish uchun qaysi atributdan foydalaniladi?`,
+    pregunta: `<b class="number">27)</b> HTMLda kiritish maydonini to'ldirish kerakligini ko'rsatish uchun qaysi atributdan foydalaniladi?`,
     respuesta: `required`,
     distractores: 
     [
@@ -270,43 +270,43 @@ baseDePreguntas = [
   ]
 },
   {
-    pregunta: `<b class="number">27)</b> O'lchov moslamasini chiqarish uchun qaysi HTML elementi ishlatiladi?`,
-    respuesta: `&lt;meter&gt;`,
+    pregunta: `<b class="number">28)</b> O'lchov moslamasini chiqarish uchun qaysi HTML elementi ishlatiladi?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">meter</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;measure&gt;`,
-    `&lt;gauge&gt;`,
-    `&lt;range&gt;`
+    `<code>&lt;</code><kbd class="teg">measure</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">gauge</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">range</kbd><code>&gt;</code>`
   ]
 },
   {
-    pregunta: `<b class="number">28)</b> Qaysi HTML elementi menyu navigatsiya yaratish uchun xamma havolalarni o'z ichiga oladi?`,
-    respuesta: `&lt;nav&gt;`,
+    pregunta: `<b class="number">29)</b> Qaysi HTML elementi menyu navigatsiya yaratish uchun xamma havolalarni o'z ichiga oladi?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">nav</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;navigate&gt;`,
-    `&lt;navigation&gt;`,
-    `&lt;a href="#"&gt;`
+    `<code>&lt;</code><kbd class="teg">navigate</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">navigation</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">a href="#"</kbd><code>&gt;</code>`
   ]
 },
   {
-    pregunta: `<b class="number">29)</b> HTML kodini bosh qism bo'limni belgilash uchun qaysi HTML elementi qo'llaniladi?`,
-    respuesta: `&lt;head&gt;`,
+    pregunta: `<b class="number">30)</b> HTML kodini bosh qism bo'limni belgilash uchun qaysi HTML elementi qo'llaniladi?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">head</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;header&gt;`,
-    `&lt;section&gt;`,
-    `&lt;top&gt;`
+    `<code>&lt;</code><kbd class="teg">header</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">section</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">top</kbd><code>&gt;</code>`
   ]
 },
   {
-    pregunta: `<b class="number">30)</b> Saytning bosh qismini yaratish uchun qaysi HTML elementi qo'llaniladi?`,
-    respuesta: `&lt;header&gt;`,
+    pregunta: `<b class="number">31)</b> Saytning bosh qismini yaratish uchun qaysi HTML elementi qo'llaniladi?`,
+    respuesta: `<code>&lt;</code><kbd class="teg">header</kbd><code>&gt;</code>`,
     distractores: 
     [
-    `&lt;section&gt;`,
-    `&lt;head&gt;`,
-    `&lt;top&gt;`
+    `<code>&lt;</code><kbd class="teg">section</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">head</kbd><code>&gt;</code>`,
+    `<code>&lt;</code><kbd class="teg">top</kbd><code>&gt;</code>`
   ]
 }
 ];
