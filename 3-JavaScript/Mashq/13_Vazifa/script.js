a9de3f0d9e14f21be48065411ba470d6ca347773
IslamDevFull <it.front.back@gmail.com>
2024-05-13 19:23:07 +0500

    0.2.60v

---
+++ 3-JavaScript/Mashq/13_Vazifa/script.js
@@ -0,0 +1,476 @@
+function headerFonToggle(){
+    document.getElementById("headerFonDown").classList.toggle("show");
+}
+function headerPaddingToggle(){
+    document.getElementById("headerPaddingDown").classList.toggle("show");
+}
+function headerAliginToggle(){
+    document.getElementById("headerAliginDown").classList.toggle("show");
+}
+function headerBorderToggle(){
+    document.getElementById("headerBorderDown").classList.toggle("show");
+}
+function headerSizeToggle(){
+    document.getElementById("headerSizeDown").classList.toggle("show");
+}
+// ***********************************Menyu************************************
+function menyuFonToggle(){
+    document.getElementById("menyuFonDown").classList.toggle("show");
+}
+function menyuPaddingToggle(){
+    document.getElementById("menyuPaddingDown").classList.toggle("show");
+}
+function menyuAliginToggle(){
+    document.getElementById("menyuAliginDown").classList.toggle("show");
+}
+function menyuBorderToggle(){
+    document.getElementById("menyuBorderDown").classList.toggle("show");
+}
+function menyuSizeToggle(){
+    document.getElementById("menyuSizeDown").classList.toggle("show");
+}
+function menyuColorToggle(){
+    document.getElementById("menyuColorDown").classList.toggle("show");
+}
+function menyuDecorToggle(){
+    document.getElementById("menyurDecorDown").classList.toggle("show");
+}
+function menyuMarginToggle(){
+    document.getElementById("menyurMarginDown").classList.toggle("show");
+}
+// ***********************************Content************************************
+function contentFonToggle(){
+    document.getElementById("contentFonDown").classList.toggle("show");
+}
+function contentPaddingToggle(){
+    document.getElementById("contentPaddingDown").classList.toggle("show");
+}
+function contentAliginToggle(){
+    document.getElementById("contentAliginDown").classList.toggle("show");
+}
+function contentBorderToggle(){
+    document.getElementById("contentBorderDown").classList.toggle("show");
+}
+function contentSizeToggle(){
+    document.getElementById("contentSizeDown").classList.toggle("show");
+}
+function contentHeightToggle(){
+    document.getElementById("contentHeightDown").classList.toggle("show");
+}
+// *************************************Style********************************************
+// *************************************Fon********************************************
+
+function bkRedColor(){
+    document.getElementById("header").style.backgroundColor = "red";
+}
+function bkGreenColor(){
+    document.getElementById("header").style.backgroundColor = "green";
+}
+function bkBlueColor(){
+    document.getElementById("header").style.backgroundColor = "blue";
+}
+function bkPowderColor(){
+    document.getElementById("header").style.backgroundColor = "powderblue";
+}
+function bkPinkColor(){
+    document.getElementById("header").style.backgroundColor = "pink";
+}
+function bkNoneColor(){
+    document.getElementById("header").style.backgroundColor = "white";
+}
+// *******************************Padding********************************************
+function bkRedPadding(){
+    document.getElementById("header").style.padding = "5px";
+}
+function bkGreenPadding(){
+    document.getElementById("header").style.padding = "20px";
+}
+function bkBluePadding(){
+    document.getElementById("header").style.padding = "35px";
+}
+function bkPowderPadding(){
+    document.getElementById("header").style.padding = "50px";
+}
+function bkPinkPadding(){
+    document.getElementById("header").style.padding = "150px";
+}
+function bkNonePadding(){
+    document.getElementById("header").style.padding = "0px";
+}
+// *******************************Align********************************************
+function bkRedAligin(){
+    document.getElementById("header").style.textAlign = "left";
+}
+function bkGreenAligin(){
+    document.getElementById("header").style.textAlign = "center";
+}
+function bkBlueAligin(){
+    document.getElementById("header").style.textAlign = "right";
+}
+
+// *******************************border********************************************
+function bkRedBorder(){
+    document.getElementById("header").style.border = "5px solid red";
+}
+function bkGreenBorder(){
+    document.getElementById("header").style.border = "5px solid green";
+}
+function bkBlueBorder(){
+    document.getElementById("header").style.border = "5px solid blue";
+}
+function bkPowderBorder(){
+    document.getElementById("header").style.border = "5px solid powderblue";
+}
+function bkPinkBorder(){
+    document.getElementById("header").style.border = "5px solid pink";
+}
+function bkNoneBorder(){
+    document.getElementById("header").style.border = "0px";
+}
+// *******************************Size********************************************
+
+function bkRedSize(){
+    document.getElementById("header").style.fontSize = "5px";
+}
+function bkGreenSize(){
+    document.getElementById("header").style.fontSize = "10px";
+}
+function bkBlueSize(){
+    document.getElementById("header").style.fontSize = "25px";
+}
+function bkPowderSize(){
+    document.getElementById("header").style.fontSize = "40px";
+}
+function bkPinkSize(){
+    document.getElementById("header").style.fontSize = "50px";
+}
+function bkNoneSize(){
+    document.getElementById("header").style.fontSize = "initial";
+}
+// ******************************************** Menyyu ********************************************************* \\
+// *************************************Fon********************************************
+
+function menyuBkRedColor(){
+    document.getElementById("menyu").style.backgroundColor = "red";
+}
+function menyuBkGreenColor(){
+    document.getElementById("menyu").style.backgroundColor = "green";
+}
+function menyuBkBlueColor(){
+    document.getElementById("menyu").style.backgroundColor = "blue";
+}
+function menyuBkGreyColor(){
+    document.getElementById("menyu").style.backgroundColor = "#666";
+}
+function menyuBkPowderColor(){
+    document.getElementById("menyu").style.backgroundColor = "powderblue";
+}
+function menyuBkPinkColor(){
+    document.getElementById("menyu").style.backgroundColor = "pink";
+}
+function menyuBkNoneColor(){
+    document.getElementById("menyu").style.backgroundColor = "white";
+}
+// *******************************Padding********************************************
+function menyuBkRedPadding(){
+    document.getElementById("menyu").style.padding = "5px";
+}
+function menyuBkGreenPadding(){
+    document.getElementById("menyu").style.padding = "20px";
+}
+function menyuBkBluePadding(){
+    document.getElementById("menyu").style.padding = "35px";
+}
+function menyuBkPowderPadding(){
+    document.getElementById("menyu").style.padding = "50px";
+}
+function menyuBkPinkPadding(){
+    document.getElementById("menyu").style.padding = "150px";
+}
+function menyuBkNonePadding(){
+    document.getElementById("menyu").style.padding = "0px";
+}
+// *******************************Align********************************************
+function menyuBkRedAligin(){
+    document.getElementById("menyu").style.textAlign = "left";
+}
+function menyuBkGreenAligin(){
+    document.getElementById("menyu").style.textAlign = "center";
+}
+function menyuBkBlueAligin(){
+    document.getElementById("menyu").style.textAlign = "right";
+}
+
+// *******************************border********************************************
+function menyuBkRedBorder(){
+    document.getElementById("menyu").style.border = "5px solid red";
+}
+function menyuBkGreenBorder(){
+    document.getElementById("menyu").style.border = "5px solid green";
+}
+function menyuBkBlueBorder(){
+    document.getElementById("menyu").style.border = "5px solid blue";
+}
+function menyuBkGreyBorder(){
+    document.getElementById("menyu").style.border = "5px solid #666";
+}
+function menyuBkPowderBorder(){
+    document.getElementById("menyu").style.border = "5px solid powderblue";
+}
+function menyuBkPinkBorder(){
+    document.getElementById("menyu").style.border = "5px solid pink";
+}
+function menyuBkNoneBorder(){
+    document.getElementById("menyu").style.border = "0px";
+}
+// *******************************Size********************************************
+
+function menyuBkRedSize(){
+    document.getElementById("menyu").style.fontSize = "5px";
+}
+function menyuBkGreenSize(){
+    document.getElementById("menyu").style.fontSize = "10px";
+}
+function menyuBkBlueSize(){
+    document.getElementById("menyu").style.fontSize = "25px";
+}
+function menyuBkPowderSize(){
+    document.getElementById("menyu").style.fontSize = "40px";
+}
+function menyuBkPinkSize(){
+    document.getElementById("menyu").style.fontSize = "50px";
+}
+function menyuBkNoneSize(){
+    document.getElementById("menyu").style.fontSize = "initial";
+}
+// *******************************Color********************************************
+
+function menyuColorRedColor(){
+    document.getElementById("men").style.color = "red";
+    document.getElementById("men1").style.color = "red";
+    document.getElementById("men2").style.color = "red";
+    document.getElementById("men3").style.color = "red";
+    document.getElementById("men4").style.color = "red";
+}
+function menyuColorGreenColor(){
+    document.getElementById("men").style.color = "green";
+    document.getElementById("men1").style.color = "green";
+    document.getElementById("men2").style.color = "green";
+    document.getElementById("men3").style.color = "green";
+    document.getElementById("men4").style.color = "green";
+}
+function menyuColorBlueColor(){
+    document.getElementById("men").style.color = "blue";
+    document.getElementById("men1").style.color = "blue";
+    document.getElementById("men2").style.color = "blue";
+    document.getElementById("men3").style.color = "blue";
+    document.getElementById("men4").style.color = "blue";
+}
+function menyuColorGreyColor(){
+    document.getElementById("men").style.color = "grey";
+    document.getElementById("men1").style.color = "grey";
+    document.getElementById("men2").style.color = "grey";
+    document.getElementById("men3").style.color = "grey";
+    document.getElementById("men4").style.color = "grey";
+}
+function menyuColorPowderColor(){
+    document.getElementById("men").style.color = "powderblue";
+    document.getElementById("men1").style.color = "powderblue";
+    document.getElementById("men2").style.color = "powderblue";
+    document.getElementById("men3").style.color = "powderblue";
+    document.getElementById("men4").style.color = "powderblue";
+}
+function menyuColorPinkColor(){
+    document.getElementById("men").style.color = "pink";
+    document.getElementById("men1").style.color = "pink";
+    document.getElementById("men2").style.color = "pink";
+    document.getElementById("men3").style.color = "pink";
+    document.getElementById("men4").style.color = "pink";
+}
+function menyuColorNoneColor(){
+    document.getElementById("men").style.color = "initial";
+    document.getElementById("men1").style.color = "initial";
+    document.getElementById("men2").style.color = "initial";
+    document.getElementById("men3").style.color = "initial";
+    document.getElementById("men4").style.color = "initial";
+}
+// *******************************Align********************************************
+function menyuBkGreyDecor(){
+    document.getElementById("men").style.textDecoration = "underline";
+    document.getElementById("men1").style.textDecoration = "underline";
+    document.getElementById("men2").style.textDecoration = "underline";
+    document.getElementById("men3").style.textDecoration = "underline";
+    document.getElementById("men4").style.textDecoration = "underline";
+}
+function menyuBkPowderDecor(){
+    document.getElementById("men").style.textDecoration = "overline";
+    document.getElementById("men1").style.textDecoration = "overline";
+    document.getElementById("men2").style.textDecoration = "overline";
+    document.getElementById("men3").style.textDecoration = "overline";
+    document.getElementById("men4").style.textDecoration = "overline";
+}
+function menyuBkNoneDecor(){
+    document.getElementById("men").style.textDecoration = "none";
+    document.getElementById("men1").style.textDecoration = "none";
+    document.getElementById("men2").style.textDecoration = "none";
+    document.getElementById("men3").style.textDecoration = "none";
+    document.getElementById("men4").style.textDecoration = "none";
+}
+// *******************************Margin********************************************
+function menyuBkRedMargin(){
+    document.getElementById("men").style.marginLeft = "5px";
+    document.getElementById("men1").style.marginLeft = "5px";
+    document.getElementById("men2").style.marginLeft = "5px";
+    document.getElementById("men3").style.marginLeft = "5px";
+    document.getElementById("men4").style.marginLeft = "5px";
+}
+function menyuBkGreenMargin(){
+    document.getElementById("men").style.marginLeft = "20px";
+    document.getElementById("men1").style.marginLeft = "20px";
+    document.getElementById("men2").style.marginLeft = "20px";
+    document.getElementById("men3").style.marginLeft = "20px";
+    document.getElementById("men4").style.marginLeft = "20px";
+}
+function menyuBkBlueMargin(){
+    document.getElementById("men").style.marginLeft = "35px";
+    document.getElementById("men1").style.marginLeft = "35px";
+    document.getElementById("men2").style.marginLeft = "35px";
+    document.getElementById("men3").style.marginLeft = "35px";
+    document.getElementById("men4").style.marginLeft = "35px";
+}
+function menyuBkPowderMargin(){
+    document.getElementById("men").style.marginLeft = "50px";
+    document.getElementById("men1").style.marginLeft = "50px";
+    document.getElementById("men2").style.marginLeft = "50px";
+    document.getElementById("men3").style.marginLeft = "50px";
+    document.getElementById("men4").style.marginLeft = "50px";
+}
+function menyuBkPinkMargin(){
+    document.getElementById("men").style.marginLeft = "150px";
+    document.getElementById("men1").style.marginLeft = "150px";
+    document.getElementById("men2").style.marginLeft = "150px";
+    document.getElementById("men3").style.marginLeft = "150px";
+    document.getElementById("men4").style.marginLeft = "150px";
+}
+function menyuBkNoneMargin(){
+    document.getElementById("men").style.marginLeft = "0px";
+    document.getElementById("men1").style.marginLeft = "0px";
+    document.getElementById("men2").style.marginLeft = "0px";
+    document.getElementById("men3").style.marginLeft = "0px";
+    document.getElementById("men4").style.marginLeft = "0px";
+}
+// *************************************Content********************************************
+// *************************************Fon********************************************
+
+function contentBkRedColor(){
+    document.getElementById("content").style.backgroundColor = "red";
+}
+function contentBkGreenColor(){
+    document.getElementById("content").style.backgroundColor = "green";
+}
+function contentBkBlueColor(){
+    document.getElementById("content").style.backgroundColor = "blue";
+}
+function contentBkGreyColor(){
+    document.getElementById("content").style.backgroundColor = "#666";
+}
+function contentBkPowderColor(){
+    document.getElementById("content").style.backgroundColor = "powderblue";
+}
+function contentBkPinkColor(){
+    document.getElementById("content").style.backgroundColor = "pink";
+}
+function contentBkNoneColor(){
+    document.getElementById("content").style.backgroundColor = "white";
+}
+// *******************************Padding********************************************
+function contentBkRedPadding(){
+    document.getElementById("content").style.padding = "5px";
+}
+function contentBkGreenPadding(){
+    document.getElementById("content").style.padding = "20px";
+}
+function contentBkBluePadding(){
+    document.getElementById("content").style.padding = "35px";
+}
+function contentBkPowderPadding(){
+    document.getElementById("content").style.padding = "50px";
+}
+function contentBkPinkPadding(){
+    document.getElementById("content").style.padding = "150px";
+}
+function contentBkNonePadding(){
+    document.getElementById("content").style.padding = "0px";
+}
+// *******************************Align********************************************
+function contentBkRedAligin(){
+    document.getElementById("content").style.textAlign = "left";
+}
+function contentBkGreenAligin(){
+    document.getElementById("content").style.textAlign = "center";
+}
+function contentBkBlueAligin(){
+    document.getElementById("content").style.textAlign = "right";
+}
+
+// *******************************border********************************************
+function contentBkRedBorder(){
+    document.getElementById("content").style.border = "5px solid red";
+}
+function contentBkGreenBorder(){
+    document.getElementById("content").style.border = "5px solid green";
+}
+function contentBkBlueBorder(){
+    document.getElementById("content").style.border = "5px solid blue";
+}
+function contentBkGreyBorder(){
+    document.getElementById("content").style.border = "5px solid #666";
+}
+function contentBkPowderBorder(){
+    document.getElementById("content").style.border = "5px solid powderblue";
+}
+function contentBkPinkBorder(){
+    document.getElementById("content").style.border = "5px solid pink";
+}
+function contentBkNoneBorder(){
+    document.getElementById("content").style.border = "0px";
+}
+// *******************************Size********************************************
+
+function contentBkRedSize(){
+    document.getElementById("content").style.fontSize = "5px";
+}
+function contentBkGreenSize(){
+    document.getElementById("content").style.fontSize = "10px";
+}
+function contentBkBlueSize(){
+    document.getElementById("content").style.fontSize = "25px";
+}
+function contentBkPowderSize(){
+    document.getElementById("content").style.fontSize = "40px";
+}
+function contentBkPinkSize(){
+    document.getElementById("content").style.fontSize = "50px";
+}
+function contentBkNoneSize(){
+    document.getElementById("content").style.fontSize = "initial";
+}
+// *******************************Height********************************************
+function contentBkRedHeight(){
+    document.getElementById("content").style.height = "50px";
+}
+function contentBkGreenHeight(){
+    document.getElementById("content").style.height = "200px";
+}
+function contentBkBlueHeight(){
+    document.getElementById("content").style.height = "350px";
+}
+function contentBkPowderHeight(){
+    document.getElementById("content").style.height = "500px";
+}
+function contentBkPinkHeight(){
+    document.getElementById("content").style.height = "1500px";
+}
+function contentBkNoneHeight(){
+    document.getElementById("content").style.height = "0px";
+}
